e707030719513c2c7bb58f6627ec88b1a1fd0af9
Gonzalo <gonzalohm93@gmail.com>
2025-02-03 20:58:39 +0100

    rafact user routes

--- api/routes/user.js
+++ api/routes/user.js
@@ -13,9 +13,5 @@ const userRoutes = express.Router()
 
 userRoutes.get('/', getUsers)
 userRoutes.get('/profile/:username', [isAuth], findUser)
-userRoutes.get('/score/:game', [isAuth], getScore)
-userRoutes.post('/register', registerUser)
-userRoutes.post('/login', loginUser)
-userRoutes.post('/update-score', [isAuth], updateScore)
 
 export default userRoutes
