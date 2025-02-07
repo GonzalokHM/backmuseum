3bd4c3c3da801b1ffb26a50aa849ec872f4dda6e
Gonzalo <gonzalohm93@gmail.com>
2025-02-07 23:42:03 +0100

    rename

--- config/jwt.js
+++ config/jwt.js
@@ -1,11 +1,11 @@
 import jwt from 'jsonwebtoken'
 
 const generateSign = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '1d' })
+  return jwt.sign({ id }, process.env.SECRET_KEY, { expiresIn: '1d' })
 }
 
 const verifyJwt = (token) => {
-  return jwt.verify(token, process.env.JWT_SECRET)
+  return jwt.verify(token, process.env.SECRET_KEY)
 }
 
 export { generateSign, verifyJwt }
